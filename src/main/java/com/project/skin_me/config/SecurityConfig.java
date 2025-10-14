bd6c3abea63b185c1d5b4601ef155207c798a6de
wayha168 <vehaseng1122@example.com>
2025-10-14 17:41:10 +0700

    All up to date all fix

--- src/main/java/com/project/skin_me/config/SecurityConfig.java
+++ src/main/java/com/project/skin_me/config/SecurityConfig.java
@@ -119,7 +119,7 @@ public class SecurityConfig {
             @Override
             public void addCorsMappings(CorsRegistry registry) {
                 registry.addMapping("/**")
-                        .allowedOrigins("http://localhost:5173")
+                        .allowedOriginPatterns("*")
                         .allowedMethods("GET", "POST", "PUT", "DELETE", "OPTIONS")
                         .allowedHeaders("*")
                         .allowCredentials(true);
