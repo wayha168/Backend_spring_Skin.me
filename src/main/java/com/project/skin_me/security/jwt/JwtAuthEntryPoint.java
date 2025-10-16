ee021c3e2213d496cb9b6a2ce001f5098b28bbfc
wayha168 <vehaseng1122@example.com>
2025-10-16 13:53:35 +0700

    all fix

--- src/main/java/com/project/skin_me/security/jwt/JwtAuthEntryPoint.java
+++ src/main/java/com/project/skin_me/security/jwt/JwtAuthEntryPoint.java
@@ -25,7 +25,7 @@ public class JwtAuthEntryPoint implements AuthenticationEntryPoint {
         response.setStatus(HttpServletResponse.SC_UNAUTHORIZED);
 
         final Map<String, Object> body = new HashMap<>();
-//        body.put("status", HttpServletResponse.SC_UNAUTHORIZED);
+        body.put("status", HttpServletResponse.SC_UNAUTHORIZED);
         body.put("error", "Unauthorized");
         body.put("message", "You may not have permission to access this resource, please login to try again!");
 //        body.put("path", request.getServletPath());
