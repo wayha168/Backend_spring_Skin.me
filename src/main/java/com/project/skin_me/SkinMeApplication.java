0dea012e821d4c6e2b6543277835dac3aa433aeb
wayha168 <vehaseng1122@example.com>
2025-10-24 14:04:00 +0700

    Fix with Gemini

--- src/main/java/com/project/skin_me/SkinMeApplication.java
+++ src/main/java/com/project/skin_me/SkinMeApplication.java
@@ -1,7 +1,5 @@
 package com.project.skin_me;
 
-import com.google.genai.Client;
-import com.google.genai.types.GenerateContentResponse;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.security.config.annotation.method.configuration.EnableMethodSecurity;
